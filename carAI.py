f6f8e09e8087d72f2a559348e09558aa55338b8d
colombo0718 <colombo0718@gmail.com>
2020-03-18 23:00:39 +0800

    learn pandas

--- carAI.py
+++ carAI.py
@@ -1,10 +1,24 @@
 import logging
 from websocket_server import WebsocketServer
+import numpy as np
+import pandas as pd
 
 key="D"
+actions=['A','S','D']
+qTable=pd.DataFrame(columns=actions)
+print(qTable,qTable.columns)
+# state="0,0,0"
+# print(state in qTable.columns)
+# qTable=qTable.append(pd.Series([0,0,0],index=qTable.columns,name=state))
+# print(qTable)
 
 def message_back(client, server, message):
+    global qTable
     print("Client(%d) said: %s" % (client['id'], message))
+    if message=="endgame":
+        print(qTable)
+        return 0
+
     # 
     rewd = int(message.split(',')[0])
     redX = int(message.split(',')[1])
@@ -20,6 +34,9 @@ def message_back(client, server, message):
         key="A"
     print("return "+key)
     server.send_message(client,key)
+
+    if state not in qTable.index :
+        qTable=qTable.append(pd.Series([0,0,0],index=qTable.columns,name=state))
 
 server = WebsocketServer(4300, host='', loglevel=logging.INFO)
 server.set_fn_message_received(message_back)
