1151457941fb7329719f1d196dd15d79751a52d7
colombo0718 <colombo0718@gmail.com>
2020-05-30 23:42:22 +0800

    success websocket random control

---
+++ goup-server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const SocketServer = require('ws').Server
+const PORT = 4300
+const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`))
+const ws = new SocketServer({ server })
+const actions=['w','s']
+
+console.log('start')
+ws.on('connection', ws => {
+    console.log('Client connected')
+
+    ws.on('message', data => {
+        console.log(data)
+        if(data=="end"){
+            console.log('aaaa')
+            setTimeout(function(){ws.send(' ')},500)
+            // ws.send(' ')
+            return 0
+            // setTimeout(function(){ws.send(' ')},500)
+        }
+        setTimeout(function(){
+            // ws.send('w')
+            ws.send(actions[Math.floor(Math.random()*actions.length)])
+        },100)
+    })
+
+    ws.on('close', () => {
+        console.log('Close connected')
+    })
+})
